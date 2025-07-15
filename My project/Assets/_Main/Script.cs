63e11732640123d015c748f7423aa7c5e99914ab
CaldoMaggi <mquiroz984@soyudemedellin.edu.co>
2025-07-15 09:37:32 -0500

    Init Unity Proyect
    Unity version 6.0.48 f1

---
+++ My project/Assets/_Main/Script.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+using TMPro;
+
+public class Script : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text _texto;
+
+    void Start()
+    {
+    
+    }
+    void Update()
+    {
+        
+    }
+}
